3ff525c0a8b2f1e708306c55245b9691efe711af
kanakof <kanakofukuda199531@gmail.com>
2023-05-06 10:26:34 +0900

    Post request改良Ver(wip2)

--- personality_backend.py
+++ personality_backend.py
@@ -6,24 +6,24 @@ app = Flask(__name__, static_url_path='',
 
 @app.route('/', methods=['GET'])
 def process_GET_request():
-    # personality_type 
-    render_template('page.html', title='personality', name="top page")
+    return render_template('page.html', title='personality', name="top page")
 
 @app.route('/', methods=['POST'])
 def process_POST_request():
     print("post request is received")
+    result_type = result_type(request)
+
+    if request.method == 'POST':
+        return render_template('page.html', result_type = result_type)
+
     # result_dictionary = {
     #     "first_question": 0,
     #     "second_question": 1,
     #     "third_question": 2,
     # }
     # result_type = process_data(result_dictionary)
-    # if request.method == 'POST':
-    #     selected_items = request.form.getlist('items')
-    #     print(selected_items)
-    #     personality_type render_template('page.html',result_type=result_type)
-
-def result():
+   
+def result_type():
     total = 0
 
     for i in range(1,11):
@@ -39,7 +39,9 @@ def result():
     elif total >=0:
         personality_type = 'type D'
     
-    return render_template('page.html', total = total)
+        # selected_items = request.form.getlist('items')
+        # print(selected_items)
+        # # personality_type render_template('page.html',result_type=result_type)
 
 # def process_data(data):
 #     """
