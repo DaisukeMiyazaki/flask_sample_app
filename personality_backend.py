d9b3ae3d5cd901b47f382049e5df9ca770be7636
kanakof <kanakofukuda199531@gmail.com>
2023-05-12 23:55:57 +0900

    redirect追加版(wip)

--- personality_backend.py
+++ personality_backend.py
@@ -15,6 +15,7 @@ def process_GET_request():
 
 """POSTリクエストがあった場合に処理する環境を整える"""
 """POSTリクエストの中身を実際に処理して返す"""
+
 @app.route('/', methods=['POST'])
 def process_POST_request():
     print("post request is received")
@@ -25,6 +26,10 @@ def process_POST_request():
         # personality_type = result_type
         print("Daisuke")
         return render_template('page.html', result = result_type )
+
+
+
+
 
 
 def get_personality_type(request,apple):
@@ -41,16 +46,36 @@ def get_personality_type(request,apple):
         if answer:
             total += int(answer)
     print(total)
+
     if total >= 40:
         personality_type = 'type A'
+        return redirect(url_for("result_a"))
     elif 30 <= total <40:
         personality_type = 'type B'
+        return redirect(url_for("result_b"))
     elif 20 <= total <30:
         personality_type = 'type C'
+        return redirect(url_for("result_c"))
     elif total >=0:
         personality_type = 'type D'
-    return personality_type
+        return redirect(url_for("result_d"))
 
+
+@app.route('/result_a.html')
+def result_a():
+    return render_template("result_a.html")
+
+@app.route('/result_b.html')
+def result_b():
+    return render_template("result_b.html")
+
+@app.route('/result_c.html')
+def result_c():
+    return render_template("result_c.html")
+
+@app.route('/result_d.html')
+def result_d():
+    return render_template("result_d.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000, threaded=True)
