b864fd979edded8fddfd2629c2cc7f2634b58eca
kanakof <kanakofukuda199531@gmail.com>
2023-05-23 17:45:43 +0900

    コメントリファクタリング

--- personality_backend.py
+++ personality_backend.py
@@ -6,17 +6,10 @@ app = Flask(__name__, static_url_path='',
             static_folder='static',
             template_folder='templates')
 
-"""htmlページをサーバーで取得する為"""
+
 @app.route('/', methods=['GET'])
 def process_GET_request():
     return render_template('front.html', title='personality', name="top page")
-
-"""POSTリクエストがあった場合に処理する環境を整える"""
-"""POSTリクエストの中身を実際に処理して返す"""
-
-# @app.route('/page.html')
-# def page():
-#     return render_template("page.html")
 
 
 @app.route('/', methods=['POST'])
@@ -25,9 +18,8 @@ def process_POST_request():
     apple = "banana"
     result_type = get_personality_type(request,apple)
 
+    """POSTの結果次第で、レンダリングする"""
     if request.method == 'POST':
-        # personality_type = result_type
-        print("Daisuke")
         print(result_type)
         print(type(result_type))
         if result_type == 'type A':
@@ -36,26 +28,16 @@ def process_POST_request():
             return redirect(url_for("result_b"))
         elif result_type == 'type C':
             return redirect(url_for("result_c"))
-        elif result_type == 'type D':
-            return redirect(url_for("result_d"))
 
-
-# def get_group_list(request):
-#     group1 = ["1","4","8","10","13","17"]
-#     group2 = ["2","6","9","12","15","18"]
-#     group3 = ["3","5","7","11","14","16"]
-
-    # return my_list
 
 def get_personality_type(request,apple):
+    """group1(=タイプA),group2(=タイプB),group3(=タイプC)"""
+    """[]内の数値はhtmlのitem名"""
     group1 = ["1","4","8","10","13","17"]
     group2 = ["2","6","9","12","15","18"]
     group3 = ["3","5","7","11","14","16"]
 
-    """group1(=タイプA),group2(=タイプB),group3(=タイプC)にはそれぞれHTMLのitem名に当たる数値を入れてリスト化
-    forinを連続することで、それぞれのタイプ別の質問Valueの合算をしている"""
-    print(apple) 
-
+    """group1(=タイプA)の合計を算出"""
     total = 0
     for i in group1:
         answer = request.form.get('item{}'.format(i))
@@ -63,18 +45,17 @@ def get_personality_type(request,apple):
         if answer:
             total += int(answer)
     print(total)
-    print("kanako")
 
+    """group2(=タイプB)の合計を算出"""
     total2 = 0
     for i in group2:
         answer = request.form.get('item{}'.format(i))
         print(answer)
         if answer:
             total2 += int(answer)
-
     print(total2)
-    print("kana")
 
+    """group3(=タイプC)の合計を算出"""
     total3 = 0
     for i in group3:
         answer = request.form.get('item{}'.format(i))
@@ -82,21 +63,13 @@ def get_personality_type(request,apple):
         if answer:
             total3 += int(answer)
     print(total3)
-    print("ka")
 
-    # for i in range(1,11):
-    #     answer = request.form.get('item{}'.format(i))
-    #     # code.interact(local=locals())
-    #     print(answer)
-    #     if answer:
-    #         total += int(answer)
-    # print(total)
-    print(max(total,total2,total3))
-
-    """total=タイプA,total2=タイプB,total3=タイプC
-    -それぞれが最も高い数値の時は、それぞれのタイプを返す
-    -数値が同一になってしまった場合は、強いタイプを優先して返す
-    （タイプＡ＞タイプＣ＞タイプＢ）"""
+    """
+    ↓Type出力ロジック説明↓
+    (前提) total=タイプA,total2=タイプB,total3=タイプC
+    (前提) タイプの強さ : タイプＡ＞タイプＣ＞タイプＢ）
+    -1つが最も高いとき : そのタイプを返す
+    -数値が同一のとき  : 強いタイプを優先して返す"""
     if total >= total2 and total3:
         personality_type = "A"
     elif total2 > total and total3:
@@ -109,19 +82,6 @@ def get_personality_type(request,apple):
         personality_type = "C"
     personality_type = "type " + personality_type
     return personality_type
-
-
-    # if total >= 40:
-    #     personality_type = "A"
-    # elif 30 <= total <40:
-    #     personality_type = "B"
-    # elif 20 <= total <30:
-    #     personality_type = "C"
-    # elif total >=0:
-    #     personality_type = "D"
-    # personality_type = "type " + personality_type
-
-    # return personality_type
 
 
 @app.route('/page.html')
